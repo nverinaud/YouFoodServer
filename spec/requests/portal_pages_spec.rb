516f49b26d9fa5727771629cdb25e7c7472068ab
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-04-11 10:41:21 +0200

    Add tests for home page

--- spec/requests/portal_pages_spec.rb
+++ spec/requests/portal_pages_spec.rb
@@ -6,7 +6,8 @@ describe 'Portal pages' do
 
   describe "Home page" do
     before { visit root_path }
-
+    it { should have_selector("img", alt: "Food picture")}
+    it { should have_selector("legend", text: "Connexion")}
   end
 
 end
