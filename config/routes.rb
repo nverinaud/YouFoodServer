060cb91a704d2985de05eff4aa040a8e3dd3803c
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-04-13 22:21:03 +0200

    Resolve conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ YouFoodServer::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   match '/director', to: 'portal#director'
-<<<<<<< .merge_file_iUTJz1
   match '/signin', to: 'sessions#new'
-=======
-  match '/signin',  to: 'sessions#new'
->>>>>>> .merge_file_9K9pc1
   match '/signout', to: 'sessions#destroy', via: :delete
 
   namespace :api do
