c51750c1c71956e5c00a54c342f0cdd07092fdfc
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-04-15 11:10:19 +0200

    Add annotations for Director, Product and User.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :integer         not null, primary key
+#  price        :decimal(, )
+#  name         :string(255)
+#  abbreviation :string(255)
+#  description  :text
+#  photoURL     :string(255)
+#  permanent    :boolean
+#  created_at   :datetime        not null
+#  updated_at   :datetime        not null
+#
+
 class Product < ActiveRecord::Base
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :integer         not null, primary key
+#  price        :decimal(, )
+#  name         :string(255)
+#  abbreviation :string(255)
+#  description  :text
+#  photoURL     :string(255)
+#  permanent    :boolean
+#  created_at   :datetime        not null
+#  updated_at   :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Product do
