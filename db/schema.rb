eacb982bc05843bfe2a59c29cc51a6d3cd0fee2b
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-04-23 22:26:13 +0200

    Add Schdule model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423103707) do
+ActiveRecord::Schema.define(:version => 20120423202519) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(:version => 20120423103707) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "category_id"
+  end
+
+  create_table "schedules", :force => true do |t|
+    t.integer  "week"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
