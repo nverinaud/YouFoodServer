bc44068954d0ed39bb7546e6db1c8ddc626e5f79
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-04-24 23:20:59 +0200

    Improve request_token api

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -1,11 +1,8 @@
 class Api::AuthenticationController < ApplicationController
   def request_token
-    puts params
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:pass])
-      render json: {
-          "youfood_remember_token" => user.remember_token
-      }
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:pass])
+      render :request_token
     else
       render text: "Mot de passe ou nom d'utilisateur incorrect", status: 403
     end
