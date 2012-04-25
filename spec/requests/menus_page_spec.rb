1762f10806dd99ba8ca3d111ca936b003a6a2cda
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-04-25 18:01:35 +0200

    Add forgotten authentication tests

--- spec/requests/menus_page_spec.rb
+++ spec/requests/menus_page_spec.rb
@@ -7,38 +7,50 @@ describe "Menus" do
   subject { page }
 
   describe "List page" do
-    before { visit menus_path }
 
-    it { should have_title "YouFood | Portail directeur | Les menus" }
+    describe "When not signed in as director" do
+      before { visit menus_path }
 
-    it { should have_selector("h1", text: "Les menus") }
-    it { should have_link("Nouveau menu") }
+      it { should_not have_title "YouFood | Portail directeur | Les menus" }
+      it { should_not have_selector("h1", text: "Les menus") }
+    end
 
-    describe "pagination" do
+    describe "When signed in as director" do
+      let(:director) { FactoryGirl.create(:director) }
       before {
-        100.times { FactoryGirl.create(:menu) }
+        sign_in director
         visit menus_path
       }
 
-      it { should have_selector("th", text: "Nom") }
-      it { should have_selector("th", text: "Nombre de produit") }
-      it { should have_selector("th", text: "Planification") }
 
-      let(:first_page) { Menu.paginate(page: 1) }
-      let(:second_page) { Menu.paginate(page: 2) }
+      it { should have_link("Nouveau menu") }
 
-      it { should have_link('Suivant') }
-      it { should have_link('2') }
+      describe "pagination" do
+        before {
+          100.times { FactoryGirl.create(:menu) }
+          visit menus_path
+        }
 
-      it "should list the first page of menus" do
-        first_page.each do |menu|
-          page.should have_link(menu.name)
+        it { should have_selector("th", text: "Nom") }
+        it { should have_selector("th", text: "Nombre de produit") }
+        it { should have_selector("th", text: "Planification") }
+
+        let(:first_page) { Menu.paginate(page: 1) }
+        let(:second_page) { Menu.paginate(page: 2) }
+
+        it { should have_link('Suivant') }
+        it { should have_link('2') }
+
+        it "should list the first page of menus" do
+          first_page.each do |menu|
+            page.should have_link(menu.name)
+          end
         end
-      end
 
-      it "should not list the second page of menus" do
-        second_page.each do |menu|
-          page.should_not have_link(menu.name)
+        it "should not list the second page of menus" do
+          second_page.each do |menu|
+            page.should_not have_link(menu.name)
+          end
         end
       end
     end
