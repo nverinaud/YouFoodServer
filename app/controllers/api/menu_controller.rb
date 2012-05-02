e7e6b87eb29174643db6b674c9eb97b7a271c8c2
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-05-02 17:37:02 +0200

    Add current week menu retrieval

--- app/controllers/api/menu_controller.rb
+++ app/controllers/api/menu_controller.rb
@@ -1,6 +1,19 @@
-class Api::MenuController < ApplicationController
+# encoding: utf-8
+
+class Api::MenuController < Api::ApiController
 
   def current_menu
-    render :current_menu
+    today = Time.now
+    Menu.all.each do |menu|
+      menu.schedules.each do |schedule|
+        if today >= schedule.start_date && today <= schedule.end_date
+          @menu = menu
+          render :current_menu
+        end
+      end
+    end
+    if (!@menu)
+      render text: "Aucun menu n'a été planifié cette semaine. Veuillez contacter le gérant du restaurant", status: 403
+    end
   end
 end
