eac0dbe00cf90c0039aa0a97dc56fb609cba5f74
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-05-04 10:11:53 +0200

    Add test for restaurant model

---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,17 @@
+require "rspec"
+
+describe Restaurant do
+  before do
+    @restaurant = Restaurant.new
+  end
+
+  it { should respond_to :manager }
+  it { should respond_to :zones }
+  it { should respond_to :tables }
+  it { should respond_to :invoices }
+  it { should respond_to :waiters }
+  it { should respond_to :cookers }
+  it { should be_valid }
+
+  
+end
