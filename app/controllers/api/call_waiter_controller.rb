884030c1336d2da46160425252fe2dfde5c5f13d
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-05-06 10:59:56 +0200

    Remove old api_helper to merge it into api/api_helper with propoer API authentication.

--- app/controllers/api/call_waiter_controller.rb
+++ app/controllers/api/call_waiter_controller.rb
@@ -2,24 +2,15 @@
 
 class Api::CallWaiterController < Api::ApiController
 
-	def call_waiter
-		if authorized?
-			table_id = params[:table_id]
-			if table_id
+	before_filter :valid_token?
 
-			else
-				show_error "Argument 'table_id' manquant.", 403
-			end
+	def call_waiter
+		table_id = params[:table_id]
+		if table_id
+			
 		else
-			show_error "Acces non autorisé.", 401
+			show_error "Argument 'table_id' manquant.", 403
 		end
 	end
-
-
-	private
-
-		def show_error(error, status)
-			render json: { error: error }, status: status
-		end
 
 end
