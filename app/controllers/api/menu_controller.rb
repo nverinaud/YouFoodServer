fb1b9223905f1cfb4086c05023ae4b05374afa91
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-05-08 13:06:11 +0200

    Fix photo url

--- app/controllers/api/menu_controller.rb
+++ app/controllers/api/menu_controller.rb
@@ -16,7 +16,7 @@ class Api::MenuController < Api::ApiController
       end
     end
     if (!@menu)
-      render text: "Aucun menu n'a été planifié cette semaine. Veuillez contacter le gérant du restaurant", status: 403
+      show_error "Aucun menu n'a été planifié cette semaine. Veuillez contacter le gérant du restaurant", 403
     end
   end
 end
