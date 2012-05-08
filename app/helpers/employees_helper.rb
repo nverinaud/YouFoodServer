cd2fe9e5cd2202233db56a79962ed85beb617215
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-05-08 10:36:50 +0200

    Add correct employee job name and remove old employees action from restaurant controller.

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,13 @@
 module EmployeesHelper
+
+	def employee_job_name(employee)
+		if employee.is_a? Cooker
+			"Cuisto"
+		elsif employee.is_a? Waiter
+			"Serveur"
+		else
+			"Autre"
+		end
+	end
+
 end
