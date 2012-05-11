eba588a741428e7c92a4de90c07778547d64ff24
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-05-11 16:14:12 +0200

    Add tables index view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120511134942) do
+ActiveRecord::Schema.define(:version => 20120511135940) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -69,8 +69,10 @@ ActiveRecord::Schema.define(:version => 20120511134942) do
   create_table "tables", :force => true do |t|
     t.string   "name"
     t.integer  "forks_nb"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.integer  "restaurant_id"
+    t.integer  "zone_id"
   end
 
   create_table "users", :force => true do |t|
