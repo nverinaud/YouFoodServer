683432ae7edf056c9073d62173ae12283e64e8af
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-05-13 15:28:36 +0200

    Refactor api helpers

--- app/controllers/api/zones_controller.rb
+++ app/controllers/api/zones_controller.rb
@@ -1,7 +1,13 @@
-class Api::ZonesController < ApplicationController
+# encoding: utf-8
+
+class Api::ZonesController < Api::ApiController
+  include RestaurantsHelper
+
+  before_filter :valid_token?, :get_restaurant, :api_valid_restaurant?
 
   #GET /api/zones
   def zones
+
     render :zones
   end
 end
--- app/helpers/api/api_helper.rb
+++ app/helpers/api/api_helper.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 module Api::ApiHelper
 
   def valid_token?
@@ -11,23 +13,23 @@ module Api::ApiHelper
     User.find_by_remember_token(token) unless token.nil?
   end
 
-  def user
-    @user ||= user_from_token_header
+  def current_user
+    @current_user ||= user_from_token_header
   end
 
-  def user=(user)
-    @user = user
+  def current_user(user)
+    @current_user = user
   end
 
   def authorized?
-    !user.nil?
+    !current_user.nil?
   end
 
   def show_error(error, status)
     render json: {error: error}, status: status
   end
 
-  def valid_restaurant?
+  def api_valid_restaurant?
     if (!@restaurant)
       show_error "Vous ne pouvez pas acceder à cette ressouce.", 401
     end
