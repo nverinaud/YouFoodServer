936884e59e08eef501978c5825b5f9004923c1f8
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-05-13 16:16:11 +0200

    Add api invoices controller and routes

---
+++ app/controllers/api/invoices_controller.rb
@@ -0,0 +1,16 @@
+class Api::InvoicesController < Api::ApiController
+
+   # GET /api/invoices
+  def index
+    show_error "Not yet implemented", 403
+  end
+
+  #PUT /api/invoices/:id
+  def update
+    show_error "Not yet implemented", 403
+  end
+  #POST /api/invoices
+  def create
+    show_error "Not yet implemented", 403
+  end
+end
