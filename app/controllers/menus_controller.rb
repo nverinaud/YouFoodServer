9e7abcf599560c7530a4b5ad83c5860fc6fcdbe4
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-06-04 20:24:29 +0200

    Add menu edition

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -21,22 +21,44 @@ class MenusController < ApplicationController
   # GET /menus/new
   def new
     @menu = Menu.new
-    @products = Product.where(permanent: false).select("name, id")
-    @schedules = Schedule.select("week, id, menu_id")
+    load_products_schedules
   end
 
   # POST /menus/create
   def create
     @menu = Menu.new(params[:menu])
-    @menu.products << Product.find(params[:products_id].split(','))
-    @menu.schedules << Schedule.find(params[:schedules_id].split(','))
+    get_products_schedules_from_params
     if @menu.save
       redirect_to menus_path
     else
       flash.now[:error] = "Une erreur est survenue."
-      @products = Product.select("name, id")
-      @schedules = Schedule.select("week, id, menu_id")
+      load_products_schedules
       render new_menu_path
+    end
+  end
+
+  # GET /menus/:id/edit
+  def edit
+    begin
+      @menu = Menu.find(params[:id])
+      load_products_schedules
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Le menu ##{params[:id]} n'existe pas."
+      redirect_to menus_path
+    end
+  end
+
+  # PUT /menus/:id
+  def update
+    @menu = Menu.find(params[:id])
+    get_products_schedules_from_params
+    if (@menu.update_attributes(params[:menu]))
+      flash[:success] = "Le menu \"#{@menu.name}\" a été mis à jour !"
+      redirect_to @menu
+    else
+      flash[:error] = "Une erreur est survenue. Essayez à nouveau."
+      load_products_schedules
+      render 'edit'
     end
   end
 
@@ -50,5 +72,16 @@ class MenusController < ApplicationController
       flash[:error] = "Le menu n'existe pas."
       redirect_to menu_path
     end
+  end
+
+  private
+  def load_products_schedules
+    @products = Product.where(permanent: false).select("name, id")
+    @schedules = Schedule.select("week, id, menu_id")
+  end
+
+  def get_products_schedules_from_params
+    @menu.products = Product.find(params[:products_id].split(','))
+    @menu.schedules = Schedule.find(params[:schedules_id].split(','))
   end
 end
