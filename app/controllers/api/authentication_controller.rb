716134b437dbf6d441b24aa457d52355604b8c23
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-06-07 10:37:27 +0200

    Add push url params to POST api/auth/request_token.

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -4,6 +4,12 @@ class Api::AuthenticationController < Api::ApiController
   def request_token
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:pass])
+      if @user.is_a?(Waiter) && !params[:push_url].blank?
+        if !@user.update_attribute :push_url, params[:push_url]
+          show_error "Erreur d'authentification (0x09255619924018264087)", 500
+          return
+        end
+      end
       render :request_token
     else
       show_error "Mot de passe ou nom d'utilisateur incorrect", 403
