4c296c2d45f55c9e014f43efe36c4be388611284
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-06-11 23:16:05 +0200

    Add webworker on the invoice page

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,43 +1,51 @@
 #encoding: utf-8
 
 class InvoicesController < ApplicationController
-	include RestaurantsHelper
+  include RestaurantsHelper
 
-	before_filter :signed_in_cooker, :get_restaurant, :valid_restaurant?
+  before_filter :signed_in_cooker, :get_restaurant, :valid_restaurant?
 
-	# GET /restaurant/invoices
+  # GET /restaurant/invoices
   def index
-  	@invoices = @restaurant.invoices.where(state: 1)
+    respond_to do |format|
+      format.html {
+        @invoices = @restaurant.invoices.where(state: 1).order('updated_at DESC')
+      }
+      format.json {
+        last_refresh = DateTime.parse(params[:since])
+        @invoices = @restaurant.invoices.where('updated_at > ? AND state = ?', last_refresh, 1)
+      }
+    end
   end
 
   # GET /restaurant/invoices/:invoice_id/call-waiter/:table_id
   def call_waiter
-  	table = Table.find(params[:table_id])
+    table = Table.find(params[:table_id])
 
-  	waiter = table.waiter
-  	if !waiter
-  		render json: {error: "Aucun serveur ne gère cette table."}, status: 500
-  		return
-  	end
+    waiter = table.waiter
+    if !waiter
+      render json: {error: "Aucun serveur ne gère cette table."}, status: 500
+      return
+    end
 
-  	if !waiter.push_url
-  		render json: {error: "Le serveur est injoignable."}, status: 500
-  		return
-  	end
+    if !waiter.push_url
+      render json: {error: "Le serveur est injoignable."}, status: 500
+      return
+    end
 
-  	options = {
-  		type: 2,
-  		invoice_id: params[:invoice_id]
-  	}
+    options = {
+        type: 2,
+        invoice_id: params[:invoice_id]
+    }
 
-  	response = MicrosoftPushNotificationService.send_notification waiter.push_url, :raw, options
+    response = MicrosoftPushNotificationService.send_notification waiter.push_url, :raw, options
 
-  	if !response.is_a? Net::HTTPSuccess
-			render json: {error: "Le serveur est injoignable."}, status: response.code
-			return
-		end
+    if !response.is_a? Net::HTTPSuccess
+      render json: {error: "Le serveur est injoignable."}, status: response.code
+      return
+    end
 
-  	render nothing: true, status: 200
+    render nothing: true, status: 200
   end
 
 end
