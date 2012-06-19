c22c0084501d24e27e3e6f01065f5acab0eddc9d
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-06-19 15:09:20 +0200

    Fix get zone api

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -3,10 +3,10 @@ class Api::RestaurantsController < Api::ApiController
   include Api::ApiRestaurantHelper
 
   before_filter :valid_token?, :get_restaurant, :api_valid_restaurant?
+  before_filter :api_valid_zone?, only: [:zones]
 
   #GET /api/zones
   def zones
-    @zones = @restaurant.zones
     render :zones
   end
 
